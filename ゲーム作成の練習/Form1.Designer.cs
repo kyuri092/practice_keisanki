293ea884a16090ad66f4d9300b1ed28990d5b94c
kyuri092 <69749831+kyuri092@users.noreply.github.com>
2020-08-27 15:24:19 +0900

    initial commit
    練習

---
+++ ゲーム作成の練習/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace ゲーム作成の練習
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.txtValue1 = new System.Windows.Forms.TextBox();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.btnChange = new System.Windows.Forms.Button();
+            this.txtValue2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(497, 11);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 0;
+            this.btnCalc.Text = "計算する";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // txtValue1
+            // 
+            this.txtValue1.Location = new System.Drawing.Point(33, 12);
+            this.txtValue1.Name = "txtValue1";
+            this.txtValue1.Size = new System.Drawing.Size(100, 23);
+            this.txtValue1.TabIndex = 1;
+            this.txtValue1.TextChanged += new System.EventHandler(this.txtValue1_TextChanged);
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(656, 12);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(100, 23);
+            this.txtAnswer.TabIndex = 3;
+            this.txtAnswer.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.listBox1.Location = new System.Drawing.Point(165, 11);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(95, 94);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(497, 56);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(50, 19);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "自動";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // btnChange
+            // 
+            this.btnChange.Location = new System.Drawing.Point(300, 53);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(75, 23);
+            this.btnChange.TabIndex = 6;
+            this.btnChange.Text = "入れ替え";
+            this.btnChange.UseVisualStyleBackColor = true;
+            this.btnChange.Click += new System.EventHandler(this.btnChange_Click);
+            // 
+            // txtValue2
+            // 
+            this.txtValue2.Location = new System.Drawing.Point(287, 11);
+            this.txtValue2.Name = "txtValue2";
+            this.txtValue2.Size = new System.Drawing.Size(100, 23);
+            this.txtValue2.TabIndex = 7;
+            this.txtValue2.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtValue2);
+            this.Controls.Add(this.btnChange);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.txtValue1);
+            this.Controls.Add(this.btnCalc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button label;
+        private System.Windows.Forms.TextBox txtValue1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button btnChange;
+        private System.Windows.Forms.TextBox txtValue2;
+    }
+}
+
---
+++ ゲーム作成の練習/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ゲーム作成の練習
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+          
+
+
+
+
+
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+
+            calc1();
+
+        }
+        private void calc1()
+        {
+
+
+            int value1 = 0;
+            int value2 = 0;
+
+            if (listBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show("演算子を入力して下さい");
+            }
+
+            if (txtValue1.Text.Equals(""))
+            {
+                MessageBox.Show("数値1を入力して下さい");
+                return;
+            }
+
+            if (txtValue2.Text.Equals(""))
+            {
+                MessageBox.Show("数値2を入力して下さい");
+                return;
+            }
+
+            try
+            {
+                value1 = int.Parse(txtValue1.Text);
+                value2 = int.Parse(txtValue2.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("エラーだo");
+                return;
+            }
+
+
+
+            int answer = 0;
+
+            if (listBox1.SelectedItem.Equals("+"))
+            {
+                answer = value1 + value2;
+            }
+
+            if (listBox1.SelectedItem.Equals("-"))
+            {
+                answer = value1 - value2;
+            }
+            if (listBox1.SelectedItem.Equals("*"))
+            {
+                answer = value1 * value2;
+            }
+            if (listBox1.SelectedItem.Equals("/"))
+            {
+                answer = value1 / value2;
+            }
+
+
+
+            txtAnswer.Text = answer.ToString();
+
+
+        }
+
+        private void calc2()
+        {
+            int value1 = 0;
+            int value2 = 0;
+
+            value1 = int.Parse(txtValue1.Text);
+            value2 = int.Parse(txtValue2.Text);
+
+
+
+            int answer = 0;
+
+            if (listBox1.SelectedItem.Equals("+"))
+            {
+                answer = value1 + value2;
+            }
+
+            if (listBox1.SelectedItem.Equals("-"))
+            {
+                answer = value1 - value2;
+            }
+            if (listBox1.SelectedItem.Equals("*"))
+            {
+                answer = value1 * value2;
+            }
+            if (listBox1.SelectedItem.Equals("/"))
+            {
+                answer = value1 / value2;
+            }
+
+
+
+            txtAnswer.Text = answer.ToString();
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                calc2();
+            }
+        }
+
+        private void txtValue1_TextChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                calc2();
+            }
+        }
+
+        private void txtValue2_TextChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                calc2();
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+
+            int change = 0;
+            int value1 = 0;
+            int value2 = 0;
+
+            change = int.Parse(txtValue1.Text);
+
+            value1 = int.Parse(txtValue2.Text);
+            value2 = change;
+
+            txtValue1.Text = value1.ToString();
+            txtValue2.Text = value2.ToString();
+           
+        }
+
+
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                calc2();
+            }
+        }
+    }
+}
